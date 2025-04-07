7534ffc77d904148d1a2e61a20cbd11f630a524b
Valeria <barretovaleria654@gmail.com>
2025-04-07 10:42:59 -0500

    usuario

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -17,3 +17,24 @@ inputs.forEach(input => {
     input.addEventListener("focus", addcl);
     input.addEventListener("blur", remcl);
 });
+
+document.getElementById('form-login').addEventListener('submit', function(e) {
+    e.preventDefault(); // Evita el envío del formulario
+  
+    const usuario = document.getElementById('user').value.trim();
+    const contrasena = document.getElementById('pass').value.trim();
+  
+    if (usuario === '' || contrasena === '') {
+      alert('Por favor ingresa usuario y contraseña');
+      return;
+    }
+  
+    // Validación con usuario y contraseña fijos
+    if (usuario === 'admin' && contrasena === '1234') {
+      // Redirige si las credenciales son correctas
+      window.location.href = 'inventario.html';
+    } else {
+      alert('Usuario o contraseña incorrectos');
+    }
+  });
+  
